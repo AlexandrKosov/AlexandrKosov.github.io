037fad0e88b344026881b322ae9e557cc9b52e32
user <user@user.us>
2020-04-10 12:43:16 +0400

    Заготовка для компонента Список элементов

---
+++ src/components/List/List.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+import './List.less';
+
+const List = ({
+    ...attrs
+}) => {
+
+	const classes = classNames(
+  
+    );
+
+    return (
+        <div>
+		!!!
+		</div>
+    )
+};
+
+List.propTypes = {
+ 
+};
+
+List.defaultProps = {
+
+};
+
+export default List;
