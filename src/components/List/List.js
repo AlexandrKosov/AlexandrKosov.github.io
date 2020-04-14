5ce98daa76b0e3c05aaf3c4f7ac762e02155e2f7
user <user@user.us>
2020-04-14 08:51:33 +0400

    переделка List и ListItem на компоненты классы для добавления стейта (активного состояния)

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,34 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 import ListItem from '~c/List/ListItem';
 import './List.less';
 
-const List = ({
-    children, className, ...attrs
-}) => {
 
-	const classes = classNames(
+class List extends Component {
+    static propTypes = {
+        children: PropTypes.node,
+        className: PropTypes.string
+    };
+  
+    static defaultProps = {
+        children: null,
+        className: ''
+    };
+  
+    state = {
+ 
+    };
+
+    render() {
+        const { className, children, ...attrs } = this.props;
+        const classes = classNames(
         'list',
         className
-    );
-
-    return (
-        <div className={classes} {...attrs}>
-            {children}
-		</div>
-    )
-};
-
-List.propTypes = {
-    children: PropTypes.node,
-    className: PropTypes.string
-};
-
-List.defaultProps = {
-    children: null,
-    className: ''
+        );
+        return (
+            <div className={classes} {...attrs}>
+                {children}
+            </div>
+        )
+    }
+	
 };
 
 export default List;
--- src/components/List/ListItem.js
+++ src/components/List/ListItem.js
@@ -1,41 +1,43 @@
 
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-const ListItem = ({
-  children,
-  className,
-  disabled,
-  active,
-  ...attrs
-}) => {
-  const classes = classNames(
-    'list-item',
-    className,
-    { disabled },
-    { active },
-  );
+class ListItem extends Component {
+  
+  static propTypes = {
+    children: PropTypes.node,
+    className: PropTypes.string,
+    disabled: PropTypes.bool,
+    active: PropTypes.bool,
+  };
+  
+  static defaultProps = {
+    children: null,
+    className: '',
+    disabled: false,
+    active: false,
+  };
 
-  return (
-    <div className={classes} {...attrs}>
-      {children}
-    </div>
-  );
-};
+  render() {
+    const {  children,
+            className,
+            disabled,
+            active,
+            ...attrs} = this.props;
+    const classes = classNames(
+      'list-item',
+      className,
+      { disabled },
+      { active },
+    );        
+    return (
+      <div className={classes} {...attrs}>
+        {children}
+      </div>
+    );
+  }
 
-ListItem.propTypes = {
-  children: PropTypes.node,
-  className: PropTypes.string,
-  disabled: PropTypes.bool,
-  active: PropTypes.bool,
-};
-
-ListItem.defaultProps = {
-  children: null,
-  className: '',
-  disabled: false,
-  active: false,
 };
 
 export default ListItem;
