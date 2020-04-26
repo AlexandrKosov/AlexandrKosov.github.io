a3a6a50c537ef9d330808b014a35ce0ea502b1ae
user <user@user.us>
2020-04-26 11:28:31 +0400

    болванка компонента <Tooltip />

---
+++ src/components/Tooltip/Tooltip.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+import './Tooltip.less';
+
+class Tooltip extends Component {
+	
+	
+	render() {
+		
+		
+		
+		return (
+			<span className="tooltip">
+				tooltip
+			</span>
+		)
+	}
+	
+}
