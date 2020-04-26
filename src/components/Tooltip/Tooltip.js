3102885fad1c573d88cfc437dcbbe1b8db6040e4
user <user@user.us>
2020-04-26 12:19:16 +0400

    Тултип - промежуточный коммит

--- src/components/Tooltip/Tooltip.js
+++ src/components/Tooltip/Tooltip.js
@@ -1,21 +1,75 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import Portal from '~c/portal';
 
 import './Tooltip.less';
 
 class Tooltip extends Component {
+	static propTypes = {
+		children: PropTypes.node.isRequired,
+		content: PropTypes.oneOfType([
+			PropTypes.string,
+			PropTypes.object,
+			PropTypes.node
+		  ]).isRequired,
+		style: PropTypes.objectOf(PropTypes.string)
+	};
 	
+	static defaultProps = {
+		style: {}
+	};
 	
+	state = {
+		visible: false
+	};
+
+    targetRef = React.createRef();
+    tooltipRef = React.createRef();
+
+	componentDidMount(){
+        const { current: targetObj } = this.targetRef;
+		const {current: tooltipObj } = this.tooltipRef;
+
+
+		let target = targetObj.getBoundingClientRect();
+		let tooltip = tooltipObj.getBoundingClientRect();
+
+		console.log(target, tooltip);
+
+
+
+	}
+
+	show = () => {
+		this.setState({visible: true});
+	}
+
+	hide = () => {
+		this.setState({visible: false});
+	}
+
 	render() {
-		
-		
+		const { children, content, style } = this.props;
+		const { visible = true } = this.state;
+
+		const classes = classNames(
+			'tooltip-element',
+			visible?'':'hidden'
+		);
 		
 		return (
-			<span className="tooltip">
-				tooltip
+			<span className="tooltip-wrapper">
+				{<Portal><span style={style} className={classes} ref={this.tooltipRef}>{content}</span></Portal> }
+				<span
+					ref={this.targetRef}
+					className="tooltip-target-element"
+					onMouseEnter={this.show}
+					onMouseLeave={this.hide}
+				>{children}</span>
 			</span>
 		)
 	}
 	
 }
+export default Tooltip;
--- src/pages/tooltips/tooltips.js
+++ src/pages/tooltips/tooltips.js
@@ -1,5 +1,10 @@
 import React, {Component} from 'react';
 
+import Tooltip from '~c/tooltip';
+
+import  Icon  from '~c/Icon';
+import  Badge  from '~c/Badge';
+
 class Tooltips extends Component{
 
     constructor (props){
@@ -12,6 +17,10 @@ class Tooltips extends Component{
         return (
            <React.Fragment>
                 <h1>Тултипы (всплывающие подсказки)</h1>
+				
+				Скажи <Tooltip content="парарарирам">слово</Tooltip> 
+                <Tooltip content={(<div><Icon name="star" size='small' />проверка<Badge variant="secondary">New</Badge></div>)}>«друг»</Tooltip> и проходи.
+				
             </React.Fragment>
        )
     }
