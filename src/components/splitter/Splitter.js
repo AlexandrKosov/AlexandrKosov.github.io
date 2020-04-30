49ff5ccd0b619dda052b089cf5ea1ac4a9677b60
user <user@user.us>
2020-04-30 11:17:32 +0400

    заготовка для компонента Сплиттер

---
+++ src/components/splitter/Splitter.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import Portal from '~c/portal';
+
+import './Splitter.less';
+
+
+const sizeTriangle = 10;
+const horizMargin = 4;
+
+class Splitter extends Component {
+	static propTypes = {
+	
+	};
+	
+	static defaultProps = {
+	};
+	
+	state = {
+	
+	};
+
+	render() {
+		return (
+			
+		)
+	}
+}
+export default Splitter;
