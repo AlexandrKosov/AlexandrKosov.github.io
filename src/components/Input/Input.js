1f8aecff155abb5818b76cfe7e1f5de81dffc5cd
user <user@user.us>
2020-05-02 10:04:10 +0400

    промежуточный коммит

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -2,22 +2,66 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-
 import './Input.less';
 
-const Input = () => {
+const Input = ({name, className, error, label, clearable, multiline, ...attrs}) => {
 	
+	const classes = classNames(
+		'text-field',
+		className,
+		{error},
+		multiline?'text-field-multiline':'',
+		clearable?'text-field-clearable':''
+	);
+
+	if(multiline) {
+	let zx=document.querySelector('.text-field-multiline');
+	if(zx)
+		console.log(zx.scrollHeight,zx.offsetHeight);
+}
+
+
+	if(attrs.type!=='text' && multiline) throw new Error('Не текстовое поле не может быть многострочным');
 	return (
-		<input type="text" />
+		<div className="text-field-wrapper">
+			{label && <label className="text-field-label" htmlFor={name}>{label}</label>}
+			{attrs.required && <span className="text-field-required">*</span>}
+			
+			{!multiline &&
+			<input name={name}
+				id={name}
+				className={classes}
+				{...attrs} />
+			}	
+			{attrs.type==='text' && multiline && 
+				<textarea name={name}
+				//ref={this.multiRef}
+				id={name}
+				className={classes}
+				{...attrs}>
+					{attrs.value}
+				</textarea>
+
+				// <div className="inner-textarea" {...attrs}>
+				// 	{attrs.value}
+				// </div>	
+			}
+			{error && <span className="text-field-error">{error}</span>}
+		</div>	
 	)
 }
 
 Input.propTypes = {
-
+	name: PropTypes.string.isRequired,
+	className: PropTypes.string,
+	label: PropTypes.string,
+	error: PropTypes.string
 };
 
 Input.defaultProps = {
-
+	className: '',
+	label: '',
+	error: ''
 };
 
 export default Input;
--- src/pages/inputs/Inputs.js
+++ src/pages/inputs/Inputs.js
@@ -8,12 +8,39 @@ class Inputs extends Component {
 	
 	
 	render() {
-		
+		const multiText = 'Тултипы сделаны таким образом, что внутри может располагаться не только какой-то текст, а еще и react-компонент. То есть туда можно поместить разнообразную верстку. Тултип автоматически рассчитывает, где ему "выпадать", сверху, или снизу. Четыре примера-кнопки по краям демонстрируют это. Этот тултип демонстрирует как раз кастомный контент выпадающей подсказки: заголовок, бейдж, цитату, автора цитаты. и кастомный текст.';
 		return (
 			<React.Fragment>
-				<div className="inputs">
-					<Input />
+			
+			<div className="inputs">
+					<Input type="text" 
+							name="word"
+							required
+							clearable
+							label="Введите слово" 
+							placeholder="Скажи слово друг и проходи" 
+							error="Сообщение об ошибке!" />
 				</div>
+				<h4>Multiline text field</h4>
+				<Input type="text" 
+							multiline
+							rows="5"
+							clearable
+							name="multi"
+							defaultValue={multiText}
+							 />
+
+				
+				<h2><span>1. Base Input type text:</span></h2>
+    <Input multiline name="text" type="text" />
+
+    <h2><span>2. Input type number (with label):</span></h2>
+    <Input name="number" label="select number" type="number" />
+
+    <h2><span>3. Input type password (with error):</span></h2>
+    <Input name="password" error="wrong password" type="password" />
+
+
 			</React.Fragment>
 		)
 	}
