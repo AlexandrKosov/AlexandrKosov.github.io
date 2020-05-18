0053e3392b6e988f184bcd55fa2633e773557c38
AlexandrKosov <62649693+AlexandrKosov@users.noreply.github.com>
2020-05-18 14:41:53 +0400

    демо контент для Collapse

--- src/pages/accordions/Accordions.js
+++ src/pages/accordions/Accordions.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
 import Collapse from '~c/collapse';
 
+import Button from '~c/Button';
+import Icon from '~c/Icon';
+import Tag from '~c/Tag';
+import Badge from '~c/Badge';
 const { Panel } = Collapse;
 
 const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
@@ -24,6 +28,27 @@ class Accordions extends Component{
                     <Panel header="This is panel header 2" key="2">
                     <p>{text}</p>
                     </Panel>
+                    <Panel header={<React.Fragment><Icon name="settings" /> Lorem Ipsum <Badge variant="danger">New</Badge></React.Fragment>} key="3">
+                    <p>
+                        <Button variant="primary">Primary</Button>
+                        <Button variant="secondary">Secondary</Button>
+                        <Button variant="success">Success</Button>
+                        <Button variant="warning">Warning</Button>
+                        <Button variant="danger">Danger</Button>
+                        <Button variant="info">Info</Button>
+                        <Button variant="light">Light</Button>
+                        <Button variant="dark">Dark</Button>
+                        <Button variant="link">Link</Button>
+                    </p>
+                    </Panel>
+                    <Panel header={<React.Fragment><Icon name="star" /> Dolor sit amet <Badge pill variant="info">New</Badge></React.Fragment>} key="4">
+                    <p>
+                    <Tag label="какой-то тег" size="large" />
+                    <Tag label="с иконкой" icon="star" size="large"/>
+                    <Tag label="с кнопкой Закрыть" withClose size="large"/>
+                    </p>
+                    </Panel>
+                    
                 </Collapse>
             </React.Fragment>
        )
