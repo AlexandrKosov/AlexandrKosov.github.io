f28029dfefbb0c743adcaa98c9af7828fc529f9c
AlexandrKosov <62649693+AlexandrKosov@users.noreply.github.com>
2020-06-05 15:46:46 +0400

    иконка и стили для нее

--- src/components/Upload/Upload.js
+++ src/components/Upload/Upload.js
@@ -110,7 +110,12 @@ class Upload extends Component {
       <React.Fragment>
         <label className="upload-container" ref={this.uploadContainerRef}  {...attrs} >
           <input type="file" ref={this.fileInputRef} onChange={this.handleSubmit} multiple />
-          <div className={classes} >Нажмите для добавления файлов, или перетащите файлы мышью</div>
+          <div className={classes} >
+          <svg className="upload-file__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="12px" height="12px" style={{opacity:"0.5"}}>
+            <path d="M286 384h-80c-14.2 1-23-10.7-24-24V192h-87.7c-17.8 0-26.7-21.5-14.1-34.1L242.3 5.7c7.5-7.5 19.8-7.5 27.3 0l152.2 152.2c11.6 11.6 3.7 33.1-13.1 34.1H320v168c0 13.3-10.7 24-24 24zm216-8v112c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-23-23V366c0-13.3 10.7-24 24-24h136v8c0 31 24.3 56 56 56h80c30.9 0 55-26.1 57-55v-8h135c13.3 0 24 10.6 24 24zm-124 88c0-11-9-20-19-20s-19 9-20 20 9 19 20 20 21-9 20-20zm64 0c0-12-9-20-20-20s-20 9-19 20 9 20 20 20 21-9 20-20z">
+            </path>
+        </svg>
+          Нажмите для добавления файлов, или перетащите файлы мышью</div>
         </label>
         <div className="upload-list-files">{list}</div> 
       </React.Fragment>
