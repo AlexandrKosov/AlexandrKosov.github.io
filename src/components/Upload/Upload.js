5844bfa9003f7140f1fefc9fc254dbbcc7832c78
AlexandrKosov <62649693+AlexandrKosov@users.noreply.github.com>
2020-06-07 16:34:02 +0400

    тестовая ветка, проба как отправлять данные о загрузке файлов через форму на сервер

--- src/components/Upload/Upload.js
+++ src/components/Upload/Upload.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import ReactDOM from 'react-dom';
+import "regenerator-runtime/runtime";
+
 import './Upload.less';
 
 import Icon from '~c/Icon';
@@ -9,11 +11,13 @@ import Icon from '~c/Icon';
 class Upload extends Component {
 	static propTypes = {
     children: PropTypes.node,
-    className: PropTypes.string
+    className: PropTypes.string,
+    onChange: PropTypes.func,
 	};
 	
 	static defaultProps = {
     children: null,
+    onChange: function(cnt){},
     className: ''
 	};
 
@@ -28,8 +32,10 @@ class Upload extends Component {
         else if(a.name<b.name) return -1
         else return 0
       });
+    
       return {fileList: arr}
-    })
+    });
+    //console.log(">",this.fileInputRef.current.files = arr);
   }
 
   onDrop = (event) => {
@@ -71,11 +77,10 @@ class Upload extends Component {
     dropZone.addEventListener("drop", this.onDrop);
   }
 
-  handleSubmit=(event)=> {
+  handleChange = async (event) => {
     event.preventDefault();
-
-    this.addToFileList(this.fileInputRef.current.files);
-
+    await this.addToFileList(this.fileInputRef.current.files);
+    this.props.onChange(this.state.fileList);
   }
 
   onDelete = (i) => {
@@ -109,7 +114,7 @@ class Upload extends Component {
     return (
       <React.Fragment>
         <label className="upload-container" ref={this.uploadContainerRef}  {...attrs} >
-          <input type="file" ref={this.fileInputRef} onChange={this.handleSubmit} multiple />
+          <input type="file" ref={this.fileInputRef} onChange={this.handleChange} multiple />
           <div className={classes} >
           <svg className="upload-file__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="12px" height="12px" style={{opacity:"0.5"}}>
             <path d="M286 384h-80c-14.2 1-23-10.7-24-24V192h-87.7c-17.8 0-26.7-21.5-14.1-34.1L242.3 5.7c7.5-7.5 19.8-7.5 27.3 0l152.2 152.2c11.6 11.6 3.7 33.1-13.1 34.1H320v168c0 13.3-10.7 24-24 24zm216-8v112c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-23-23V366c0-13.3 10.7-24 24-24h136v8c0 31 24.3 56 56 56h80c30.9 0 55-26.1 57-55v-8h135c13.3 0 24 10.6 24 24zm-124 88c0-11-9-20-19-20s-19 9-20 20 9 19 20 20 21-9 20-20zm64 0c0-12-9-20-20-20s-20 9-19 20 9 20 20 20 21-9 20-20z">
--- src/pages/UploadFiles/UploadFiles.js
+++ src/pages/UploadFiles/UploadFiles.js
@@ -7,10 +7,59 @@ import Button from '~c/Button';
 
 class UploadFiles extends Component {
 	
+	getFileList = (e) => {
+		console.log(e);
+		return e;
+	}
+
+	state = {
+		fileList:[]
+	}
+
+	onChange = (value) => {
+		console.log("val:",value);
+		this.setState({fileList: value})
+		}
+
+	onSubmit = (e) => {
+		const {fileList} = this.state;
+		e.preventDefault();
+		// создание объекта FormData 
+		let fData = new FormData();
+		//let fData = this.state.fileList;
+		console.log(fileList);
+                // Добавление файлов input type file 
+                // let file = el.querySelector('.upload-file__input');
+                // for (let i = 0; i < (file.files.length); i++) {
+                //     fData.append('files[]', file.files[i]); // добавляем файлы в объект FormData() 
+				// }
+				//console.log(">>",this.getFileList());
+
+
+
+/*
+// HTML file input, chosen by user
+formData.append("userfile", fileInputElement.files[0]);
+*/
+
+		fileList.forEach((file)=>{
+			console.log("+>",file.name, file);
+			fData.append(file.name, file, file.name);
+			console.log("*",fData.get(file.name));
+		});
+
+		console.log("fData:",fData);
+		this.setState({fileList:[]});
+		return false;
+	}
 
 	render() {
 		return (
-			<Upload />
+			<form onSubmit={this.onSubmit} >
+				<Upload onChange={this.onChange}/>
+				<input type="submit" value="Отправить" />
+			</form>
+			
 		)
 	}
 	
